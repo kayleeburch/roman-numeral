210203739e453429b30464ea200dc540ca4321c6
kayleeburch <kayleemburch@gmail.com>
2022-05-25 16:04:53 -0400

    python solution

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,10 @@
 def to_roman(num):
-    # write your code here!
+    result = []
+    dict = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1};
+    for x in dict:
+        roman = x
+        arabicNum = dict[x]
+        while(num >= arabicNum):
+            result.append(roman)
+            num = num - arabicNum
+    return "".join(result)
--- python/roman_numerals_spec.py
+++ python/roman_numerals_spec.py
@@ -3,4 +3,11 @@ from roman_numerals import to_roman
 print(to_roman(1) == 'I')
 print(to_roman(3) == 'III')
 print(to_roman(4) == 'IV')
+print(to_roman(9) == 'IX')
+print(to_roman(14) == 'XIV')
+print(to_roman(44) == 'XLIV')
+print(to_roman(944) == 'CMXLIV')
+print(to_roman(150) == 'CL')
+print(to_roman(4000) == 'MMMM')
+
 # add tests to cover different edge cases
